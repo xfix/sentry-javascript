5eab0cc277f6719f589fad3d597fb0b28215117f
Katie Byers <lobsterkatie@gmail.com>
2022-04-20 21:06:08 -0700

    fix nextjs config, add `externals` option

--- packages/nextjs/rollup.npm.config.js
+++ packages/nextjs/rollup.npm.config.js
@@ -1,3 +1,10 @@
 import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup/index.js';
 
-export default makeNPMConfigVariants(makeBaseNPMConfig());
+export default makeNPMConfigVariants(
+  makeBaseNPMConfig({
+    // We need to include `instrumentServer.ts` separately because it's only conditionally required, and so rollup
+    // doesn't automatically include it when calculating the module dependency tree.
+    entrypoints: ['src/index.server.ts', 'src/index.client.ts', 'src/utils/instrumentServer.ts'],
+    externals: ['next/router'],
+  }),
+);
