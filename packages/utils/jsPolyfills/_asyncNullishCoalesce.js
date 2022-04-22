9ca12ee25e283e0715b751329d4cd701ad8895dd
Katie Byers <lobsterkatie@gmail.com>
2022-04-22 15:44:47 -0700

    try using export for asyncnullishcoalesce

--- packages/utils/jsPolyfills/_asyncNullishCoalesce.js
+++ packages/utils/jsPolyfills/_asyncNullishCoalesce.js
@@ -1,2 +1,6 @@
 // by checking for loose equality to `null`, we catch both `null` and `undefined`
-module.exports = { _asyncNullishCoalesce: async (lhs, rhsFn) => (lhs != null ? lhs : rhsFn()) };
+// module.exports = { _asyncNullishCoalesce: async (lhs, rhsFn) => (lhs != null ? lhs : rhsFn()) };
+
+export async function _asyncNullishCoalesce(lhs, rhsFn) {
+  return lhs != null ? lhs : rhsFn();
+}
