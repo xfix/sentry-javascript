5ebd7a4894f6d3da14fe5adb02942d0a2b4c9aad
Katie Byers <lobsterkatie@gmail.com>
2022-04-22 14:04:09 -0700

    fixup polyfills

--- packages/utils/jsPolyfills/index.js
+++ packages/utils/jsPolyfills/index.js
@@ -1,12 +1,14 @@
-module.exports._asyncNullishCoalesce = require('./_asyncNullishCoalesce.js')._asyncNullishCoalesce;
-module.exports._asyncOptionalChain = require('./_asyncOptionalChain.js')._asyncOptionalChain;
-module.exports._asyncOptionalChainDelete = require('./_asyncOptionalChainDelete.js')._asyncOptionalChainDelete;
-module.exports._createNamedExportFrom = require('./_createNamedExportFrom.js')._createNamedExportFrom;
-module.exports._createStarExport = require('./_createStarExport.js')._createStarExport;
-module.exports._interopDefault = require('./_interopDefault.js')._interopDefault;
-module.exports._interopNamespace = require('./_interopNamespace.js')._interopNamespace;
-module.exports._interopRequireDefault = require('./_interopRequireDefault.js')._interopRequireDefault;
-module.exports._interopRequireWildcard = require('./_interopRequireWildcard.js')._interopRequireWildcard;
-module.exports._nullishCoalesce = require('./_nullishCoalesce.js')._nullishCoalesce;
-module.exports._optionalChain = require('./_optionalChain.js')._optionalChain;
-module.exports._optionalChainDelete = require('./_optionalChainDelete.js')._optionalChainDelete;
+module.exports = {
+  _asyncNullishCoalesce: require('./_asyncNullishCoalesce.js')._asyncNullishCoalesce,
+  _asyncOptionalChain: require('./_asyncOptionalChain.js')._asyncOptionalChain,
+  _asyncOptionalChainDelete: require('./_asyncOptionalChainDelete.js')._asyncOptionalChainDelete,
+  _createNamedExportFrom: require('./_createNamedExportFrom.js')._createNamedExportFrom,
+  _createStarExport: require('./_createStarExport.js')._createStarExport,
+  _interopDefault: require('./_interopDefault.js')._interopDefault,
+  _interopNamespace: require('./_interopNamespace.js')._interopNamespace,
+  _interopRequireDefault: require('./_interopRequireDefault.js')._interopRequireDefault,
+  _interopRequireWildcard: require('./_interopRequireWildcard.js')._interopRequireWildcard,
+  _nullishCoalesce: require('./_nullishCoalesce.js')._nullishCoalesce,
+  _optionalChain: require('./_optionalChain.js')._optionalChain,
+  _optionalChainDelete: require('./_optionalChainDelete.js')._optionalChainDelete,
+};
