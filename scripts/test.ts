6d6a5c91abdf173c2e49fa263e0162d54afa32d1
Katie Byers <lobsterkatie@gmail.com>
2022-04-22 08:10:38 -0700

    STASH changes to test ci script

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,15 +1,17 @@
 import { spawnSync } from 'child_process';
-import { join } from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 
 function run(cmd: string, cwd: string = '') {
-  const result = spawnSync(cmd, { shell: true, stdio: 'inherit', cwd: join(__dirname, '..', cwd || '') });
+  const result = spawnSync(cmd, { shell: true, stdio: 'inherit', cwd: path.join(__dirname, '..', cwd || '') });
 
   if (result.status !== 0) {
     process.exit(result.status || undefined);
   }
 }
 
-const nodeMajorVersion = parseInt(process.version.split('.')[0].replace('v', ''), 10);
+const nodeMajorVersion = 8;
+// const nodeMajorVersion = parseInt(process.version.split('.')[0].replace('v', ''), 10);
 
 // Ember tests require dependency changes for each set of tests, making them quite slow. To compensate for this, in CI
 // we run them in a separate, parallel job.
@@ -18,7 +20,7 @@ let ignorePackages = ['@sentry/ember'];
 // install legacy versions of third-party packages whose current versions don't support node 8 or 10, and skip testing
 // our own packages which don't support node 8 for various syntax or dependency reasons
 if (nodeMajorVersion <= 10) {
-  let legacyDependencies;
+  let legacyDependencies: string[];
 
   if (nodeMajorVersion === 8) {
     legacyDependencies = [
@@ -39,11 +41,40 @@ if (nodeMajorVersion <= 10) {
       '@sentry/nextjs',
     ];
 
-    // This is a hack, to deal the fact that the browser-based tests fail under Node 8, because of a conflict buried
-    // somewhere in the interaction between our current overall set of dependencies and the older versions of a small
-    // subset we're about to install below. Since they're browser-based, these tests are never going to be running in a
-    // node 8 environment in any case, so it's fine to skip them here. (In the long run, we should only run such tests
-    // against a single version of node, but in the short run, this at least allows us to not be blocked by the
+    // The ts-jest config schema changed between v25 and v26. Because we're about to downgrade to v25, we have to adjust
+    // our config to match.
+
+    run('yarn tsc --allowJs --skipLibCheck --declaration --emitDeclarationOnly jest/jest.config.js');
+
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const jestConfig = require('../jest/jest.config.js');
+
+    // We need to cast the  new `astTransformers` value to `any` because we're now violating the new schema, on which
+    // our automatically-generated types file is based.
+    jestConfig.globals['ts-jest'].astTransformers = jestConfig.globals['ts-jest'].astTransformers.after as any;
+
+    // The rest of the fixes we need to make are more easily done on the stringified code
+    let code = `module.exports = ${JSON.stringify(jestConfig, null, 2)}\n`;
+
+    // When we imported the jest config above, all expressions it contained were evaluated. Specifically, the
+    // `process.cwd()` call was replaced with a hard-coded path, which will break as soon as the jest config needs to be
+    // used in a different directory than this one. Though it's a little brute-force-y, the easiest way to fix this is
+    // just to perform the substitution in reverse.
+    const cwd = process.cwd();
+    code = code.replace(`"${cwd}"`, 'process.cwd()');
+
+    // Finally, it seems older versions of ts-jest need the transformer to be in JS from the start (newer versions will
+    // transpile it on their own), so we need to both compile it and change the extension in the filename in our config.
+    run('yarn tsc --skipLibCheck jest/constReplacer.ts');
+    code = code.replace('constReplacer.ts', 'constReplacer.js');
+
+    fs.writeFileSync(path.resolve('jest/jest.config.js'), code);
+
+    // This is a hack, to deal the fact that the browser-based tracing tests fail under Node 8, because of a conflict
+    // buried somewhere in the interaction between our current overall set of dependencies and the older versions of a
+    // small subset we're about to install below. Since they're browser-based, these tests are never going to be running
+    // in a node 8 environment in any case, so it's fine to skip them here. (In the long run, we should only run such
+    // tests against a single version of node, but in the short run, this at least allows us to not be blocked by the
     // failures.)
     run('rm -rf packages/tracing/test/browser');
   }
